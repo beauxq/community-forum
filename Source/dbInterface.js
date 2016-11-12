660194faeb89ab0a115bd00278fda2746fadc08d
beauxq <beauxq@users.noreply.github.com>
2016-11-12 17:49:00 -0600

    database interface

---
+++ Source/dbInterface.js
@@ -0,0 +1,59 @@
+/**
+ *  this module can be replaced with an interface to any database
+ *
+ *  just implement all the functions attached to "exports"
+ */
+
+var DATABASE_URL = "mongodb://beauxq:beauxq@ds051873.mlab.com:51873/tut8test";
+var COLLECTION = "hackathon";
+
+var MongoClient = require('mongodb').MongoClient;
+
+// callback takes boolean parameter = successful post
+exports.newPost = function(post, callback) {
+    MongoClient.connect(DATABASE_URL, function (err, db) {
+        if (err) {
+            console.log("error connecting to database for new post: ");
+            console.log(err);
+            callback(false);
+            return;
+        }
+
+        // TODO: assign an id
+        
+        db.collection(COLLECTION).insertOne(post, function (err) {
+            if (err) {
+                console.log("error inserting post:");
+                console.log(err);
+                callback(false);
+                return;
+            }
+
+            console.log("succesful insert");
+            callback(true);
+        })
+    });
+};
+
+// callback takes the post as a parameter
+exports.getPost = function (id, callback) {
+    MongoClient.connect(DATABASE_URL, function(err, db) {
+        if (err) {
+            console.log("error connecting to database:");
+            console.log(err);
+            callback({});
+        }
+
+        db.collection(COLLECTION).findOne({'id':id}, function(err, document) {
+            if (err) {
+                console.log("error in findOne:");
+                console.log(err);
+                callback({});
+            }
+
+            console.log("getPost got this post from database:");
+            console.log(document);
+            callback(document);
+        });
+    });
+};
--- Source/server.js
+++ Source/server.js
@@ -2,6 +2,8 @@
  * NodeJS server
  */
 
+var NEW_POST_URL = "/newpost";
+var GET_POST_URL = "/getpost";
 var PORT = 9081;
 var StatusEnum = Object.freeze({
     SUCCESS: 200,
@@ -14,25 +16,37 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var cors = require("cors");
 
+var databaseInterface = require("./dbInterface.js");
+
 var application = express();
 
 application.use(cors());
 application.use(bodyParser.json());
 application.use(bodyParser.urlencoded({ extended: true }));
 
-application.post("/newpost", function (req, res) {
+application.post(NEW_POST_URL, function (req, res) {
     var post = req.body;
 
-    console.log("newpost: " + post);
+    console.log("newpost:");
+    console.log(post);
 
-    res.write("success");
-    res.end();
+    databaseInterface.newPost(post, function (success) {
+        if (success) {
+            res.write("success");
+            res.end();
+        }
+        else {
+            res.status(StatusEnum.SERVER_ERROR);
+            res.write("error creating new post");
+            res.end();
+        }
+    });
 });
 
 /**
  *  post object with "id" attribute
  */
-application.post("/getpost", function (req, res) {
+application.post(GET_POST_URL, function (req, res) {
     var id = req.body.id;
 
     if (id == undefined) {
@@ -43,9 +57,10 @@ application.post("/getpost", function (req, res) {
     }
 
     console.log("getpost id: " + id);
-
-    res.write( JSON.stringify({'text': "Hi"}) );
-    res.end();
+    databaseInterface.getPost(id, function(result) {
+        res.write(JSON.stringify(result));
+        res.end();
+    });
 });
 
 application.use(express.static("public"));
